b5b312a8db7bd47f9b0a2ca7dd634930e19d9fbb
yunjjun <yunjjun0504@gmail.com>
2021-12-07 09:33:37 +0900

    first commit

--- app/api.py
+++ app/api.py
@@ -10,8 +10,6 @@ app = FastAPI()
 scaler = load(ROOT_DIR / "artifacts/scaler.joblib")
 model = load(ROOT_DIR / "artifacts/model.joblib")
 
-instrumentator.instrument(app).expose(app, include_in_schema=False, should_gzip=True)
-
 @app.get("/")
 def root():
     return "Wine Quality Ratings"
