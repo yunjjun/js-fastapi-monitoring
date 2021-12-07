32d4c379c6fff8265cacd14ee812ad5725ed33d0
yunjjun <yunjjun0504@gmail.com>
2021-12-07 09:32:07 +0900

    first commit

--- app/api.py
+++ app/api.py
@@ -3,7 +3,6 @@ import numpy as np
 from fastapi import FastAPI, Response
 from joblib import load
 from .schemas import Wine, Rating, feature_names
-from .monitoring import instrumentator
 
 ROOT_DIR = Path(__file__).parent.parent
 
