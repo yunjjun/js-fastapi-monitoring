27e63da0e6e0b2b298ad0c2bce6b9c4f131b99c5
yunjjun <yunjjun0504@gmail.com>
2021-12-07 16:15:11 +0900

    changed model loading part to use azureml

--- app/api.py
+++ app/api.py
@@ -3,12 +3,27 @@ import numpy as np
 from fastapi import FastAPI, Response
 from joblib import load
 from .schemas import Wine, Rating, feature_names
+from azureml.core import Workspace
+from azureml.core.model import Model
 
 ROOT_DIR = Path(__file__).parent.parent
 
 app = FastAPI()
-scaler = load(ROOT_DIR / "artifacts/scaler.joblib")
-model = load(ROOT_DIR / "artifacts/model.joblib")
+# scaler = load(ROOT_DIR / "artifacts/scaler.joblib")
+# model = load(ROOT_DIR / "artifacts/model.joblib")
+
+ws = Workspace(subscription_id="0dfd6360-d4a6-4d90-b642-22bc52ee4a2b",
+               resource_group="azure-mlops",
+               workspace_name="ml-pipeline")
+model = Model(
+    ws, 
+    'wine_model', 
+    version=3)
+scaler = Model(
+    ws, 
+    'wine_scaler', 
+    version=3
+)
 
 @app.get("/")
 def root():
