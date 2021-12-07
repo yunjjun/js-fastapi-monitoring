4ee38faa2f3f6e9d521614068bafe5d0552fed18
yunjjun <yunjjun0504@gmail.com>
2021-12-07 21:07:56 +0900

    load after downloading model and scaler

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import numpy as np
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException, status
 from joblib import load
 from .schemas import Wine, Rating, ModelName, Version, feature_names
 from azureml.core import Workspace
@@ -20,32 +20,34 @@ ws = Workspace(subscription_id="0dfd6360-d4a6-4d90-b642-22bc52ee4a2b",
 def root():
     return "Wine Quality Ratings !!"
 
-
 @app.post("/predict", response_model=Rating)
-# def predict(response: Response, sample: Wine, model_name: ModelName, version: Version):
-def predict(response: Response, sample: Wine):
+def predict(response: Response, sample: Wine, model_name: ModelName, version: Version):
     sample_dict = sample.dict()
     features = np.array([sample_dict[f] for f in feature_names]).reshape(1, -1)
     model = Model(
         ws, 
-        f"wine_model", 
-        version=4)
+        f"{model_name}_model", 
+        version=version)
     scaler = Model(
         ws, 
-        f"wine_scaler", 
-        version=4
+        f"{model_name}_scaler", 
+        version=version
     )
-    model.download(target_dir="artifacts/", exist_ok= True)
-    scaler.download(target_dir="artifacts/", exist_ok= True)
-    
-    scaler = load(ROOT_DIR / "artifacts/scaler.joblib")
-    model = load(ROOT_DIR / "artifacts/model.joblib")
-    
-    features_scaled = scaler.transform(features)
-    prediction = model.predict(features_scaled)[0]
-    response.headers["X-model-score"] = str(prediction)
-    return Rating(quality=prediction)
-
+    try:
+        model.download(target_dir="artifacts/", exist_ok= True)
+        scaler.download(target_dir="artifacts/", exist_ok= True)
+        scaler = load(ROOT_DIR / f"artifacts/{model_name}_scaler.joblib")
+        model = load(ROOT_DIR / f"artifacts/{model_name}_model.joblib")
+        
+        features_scaled = scaler.transform(features)
+        prediction = model.predict(features_scaled)[0]
+        response.headers["X-model-score"] = str(prediction)
+        return Rating(quality=prediction)
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="This model is not exists",
+        )   
 
 @app.get("/healthcheck")
 def healthcheck():
