02370617de4bb01375a9862c44f3cae151e53ba5
yunjjun <yunjjun0504@gmail.com>
2021-12-07 22:44:29 +0900

    testing original app

--- app/api.py
+++ app/api.py
@@ -3,8 +3,8 @@ import numpy as np
 from fastapi import FastAPI, Response, HTTPException, status
 from joblib import load
 from .schemas import Wine, Rating, feature_names
-from azureml.core import Workspace
-from azureml.core.model import Model
+# from azureml.core import Workspace
+# from azureml.core.model import Model
 
 ROOT_DIR = Path(__file__).parent.parent
 
@@ -12,38 +12,38 @@ app = FastAPI()
 # scaler = load(ROOT_DIR / "artifacts/scaler.joblib")
 # model = load(ROOT_DIR / "artifacts/model.joblib")
 
-ws = Workspace(subscription_id="0dfd6360-d4a6-4d90-b642-22bc52ee4a2b",
-               resource_group="azure-mlops",
-               workspace_name="ml-pipeline")
+# ws = Workspace(subscription_id="0dfd6360-d4a6-4d90-b642-22bc52ee4a2b",
+#                resource_group="azure-mlops",
+#                workspace_name="ml-pipeline")
 
 @app.get("/")
 def root():
     return "Wine Quality Ratings !!!"
 
-@app.post("/update_model")
-def update_model(model_name: str, version: int):
-    model = Model(
-        ws, 
-        f"{model_name}_model", 
-        version=version)
-    scaler = Model(
-        ws, 
-        f"{model_name}_scaler", 
-        version=version
-    )
-    try:
-        model.download(target_dir="artifacts/", exist_ok= True)
-        scaler.download(target_dir="artifacts/", exist_ok= True)
-        return {
-            "status" : "success",
-            "model_name" : model_name,
-            "version" : version
-        }
-    except:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="This model is not exists",
-        )   
+# @app.post("/update_model")
+# def update_model(model_name: str, version: int):
+#     model = Model(
+#         ws, 
+#         f"{model_name}_model", 
+#         version=version)
+#     scaler = Model(
+#         ws, 
+#         f"{model_name}_scaler", 
+#         version=version
+#     )
+#     try:
+#         model.download(target_dir="artifacts/", exist_ok= True)
+#         scaler.download(target_dir="artifacts/", exist_ok= True)
+#         return {
+#             "status" : "success",
+#             "model_name" : model_name,
+#             "version" : version
+#         }
+#     except:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND,
+#             detail="This model is not exists",
+#         )   
 
 @app.post("/predict", response_model=Rating)
 def predict(response: Response, sample: Wine, model_name: str):
